dc6b624145bee0359edce66e2cf65ef9c5c996c2
YuraYeliseev <w1ckaero@gmail.com>
2018-08-17 17:41:57 +0300

    added pymysql DB for the further usage

--- app/app.py
+++ app/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from modules.VideoProcessor import VideoProcessor
+import pymysql.cursors
+import threading
 import cv2
 
 app = Flask(__name__)
+
+connection = pymysql.connect(host="localhost",
+                             user="root",
+                             password="",
+                             db="dvd_collection",
+                             chartset="utf8mb4",
+                             cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/')
 def upload_file_render():
@@ -16,25 +25,32 @@ def upload_file():
         f = request.files['file']
         f.save(secure_filename(f.filename))
         proc= VideoProcessor('{}'.format(f.filename))
-        return stats(proc)
+        return render_template('chart.html', values=proc.graph_data,
+                               labels=proc.time)
 
-@app.route('/uploader/statistic')
+
+@app.route("/uploader/statistic")
 def stats(proc):
     grabbed, frame = proc.get_next_frame()
     n = 0
-    while grabbed:
-        proc.time_change(n, proc.stream.get(cv2.CAP_PROP_POS_MSEC))
-        if n % 220 == 0:
-            proc.make_heatmap(frame, n)
-        else:
-            proc.make_heatmap(frame)
-        # self.process_frame(frame, preview=True, crop=False)
-        # cv2.imshow("frame", frame)
-        grabbed, frame = proc.get_next_frame()
-        n += 1
-        if n == 1000:
-            break
-    return render_template('chart.html', values=proc.graph_data, labels=proc.time)
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO `table1` (`x`, `y`) VALUES (%s, %s)"
+            while grabbed:
+                proc.time_change(n, proc.stream.get(cv2.CAP_PROP_POS_MSEC))
+                if n % 220 == 0:
+                    proc.make_heatmap(frame, n)
+                else:
+                    proc.make_heatmap(frame)
+                # self.process_frame(frame, preview=True, crop=False)
+                # cv2.imshow("frame", frame)
+                cursor.execute(sql, (proc.graph_data[-1], proc.time[-1]))
+                grabbed, frame = proc.get_next_frame()
+                n += 1
+                if n == 1000:
+                    break
+    finally:
+        connection.close()
 
 
 
